a59cdaedd6dbf5a925345643856ca2f8f8cecebc
gabrovex <vecelli@gmail.com>
2019-01-08 13:41:39 +0100

    Add adapter functions to encryption service

--- src/Service/EncryptionService.php
+++ src/Service/EncryptionService.php
@@ -58,6 +58,29 @@ class EncryptionService
     }
 
     /**
+     * @param string $tableName
+     * @param string $colName
+     *
+     * @return string
+     */
+    public function getBlindIndexName($tableName = "", $colName = ""): string
+    {
+        return $this->getAdapter()->getBlindIndexName($tableName, $colName);
+    }
+
+    /**
+     * @param string $data
+     * @param string $tableName
+     * @param string $colName
+     *
+     * @return array
+     */
+    public function prepareForStorage(string $data, string $tableName = "", string $colName = ""): array
+    {
+        return $this->getAdapter()->prepareForStorage($data, $tableName, $colName);
+    }
+
+    /**
      * @return EncryptionInterface
      */
     protected function getAdapter() : EncryptionInterface
